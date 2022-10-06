e9f9d8e46159f3b9dd39edb2e02ff06c79f34eb8
gabriel202010406 <gabriel202010406@itla.edu.do>
2022-10-06 14:49:38 -0400

    Actualización de los ejercicios

--- Programas.c/Ejemplos de capitulo 3/Ejemplo 3.1.c
+++ Programas.c/Ejemplos de capitulo 3/Ejemplo 3.1.c
@@ -1,4 +1,4 @@
-#include <studio.h>
+#include <stdio.h>
 
 /*Nomina.
 el porgrama, al recibir los salarios de 15 profesores,
@@ -9,14 +9,16 @@ Sal y NOM: variable de tipo real .*/
 
 void main(void)
 {
-	int ;
-	float sal, NOM;
+	int I ;
+	float SAL, NOM;
 	NOM = 0;
-	for (I=1 ; I<=15; I++)
+	for (I=1; I<=15; I++)
+	
 	{
-		printf ('\Ingrese el salario del profesor%d:\t" , I);
-		scanf("%f",&SAL);
+		
+		printf ("Ingrese el salario del profesor%d:/t", I);
+		scanf("%f",SAL);
 		NOM = NOM + SAL ;
 	}
-	printd("\nEl total de la nomina es: %.2f", NOM);
+	printf("/El total de la nomina es: %.2f", NOM);
 }
